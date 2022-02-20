d5ae49e30a6aeeca581007428e6524791bb091fc
NUAR13 <moshkov.m@mail.ru>
2022-02-20 13:24:04 +0400

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function randomNumber(min, max) {
+  let random = min + Math.random() * (max + 1 - min);
+  return Math.floor(random);
+};
+
+randomNumber(2,11);
+
+function getLineString (string, maxChars) {
+  return string.length <= maxChars;
+ }
+getLineString("Hello World!",10);
